f067ac0da46ab2cc023e29c440dd4c3fd730a5cb
S BHARATH <141322335+Bharath05ssp@users.noreply.github.com>
2024-10-14 18:20:39 +0530

    Add files via upload

---
+++ firstproject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  firstproject
+//
+//  Created by S Bharath on 06/10/24.
+//
+
+#include <iostream>
+
+int main
+{
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
